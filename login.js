c221e9ad6a4245638a85eded5f2b25ab18236596
nisanth-shanmugam <155433865+nisanth-shanmugam@users.noreply.github.com>
2024-01-09 15:16:42 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,9 @@
+const signInBtnLink = document.querySelector('.signInBtnLink');
+const signUpBtnLink = document.querySelector('.signUpBtnLink');
+const wrapper = document.querySelector('.wrapper');
+signUpBtnLink.addEventListener('click',() =>{
+    wrapper.classList.toggle('active');
+});
+signUpBtnLink.addEventListener('click',() =>{
+    wrapper.classList.toggle('active');
+});
